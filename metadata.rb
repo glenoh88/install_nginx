3cb67080c615da43d941b4b65cf6426f3b0915bb
glenoh88 <glenoh88@gmail.com>
2015-10-22 13:38:57 -0700

    First Commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'install_nginx'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures install_nginx'
+long_description 'Installs/Configures install_nginx'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: install_nginx
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
